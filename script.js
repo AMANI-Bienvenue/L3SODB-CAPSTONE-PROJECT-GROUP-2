b4162ff490e873c5e536685a8b2d4e1c21e68320
AMANI-bienvenue <mnbienvenue@gmail.com>
2026-05-13 11:11:15 +0200

    The first files

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    alert("Welcome to Rich Motors - Your Premium Automotive Destination!");
+}
+
+function buyCar(carName) {
+    alert("Thank you for your interest in " + carName + ". Our team will contact you shortly!");
+}
